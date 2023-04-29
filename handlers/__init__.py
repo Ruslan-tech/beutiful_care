cee2f548843137e7fffab90734b3ac17166d8d69
ruslan <flash.ifmo@gmail.com>
2023-04-29 22:34:29 +0300

    first commit

---
+++ handlers/__init__.py
@@ -0,0 +1,4 @@
+from . import admin
+from . import client
+from . import common
+from . import master
---
+++ keyboards/__init__.py
@@ -0,0 +1,4 @@
+from . import client_kb
+from . import admin_kb
+from . import common_kb
+from . import master_kb
